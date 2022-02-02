4dda0e4eb77f4387d2667826ba7a56fb1aeeccf5
vmagelo <51368918+vmagelo@users.noreply.github.com>
2022-02-02 15:34:25 +0100

    Add in storage pieces.

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import ContainerClient, __version__
+
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
@@ -20,6 +23,23 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+
+@app.route('/list', methods=['POST'])
+def list():
+   storage_url = 'https://msdocspythonflaskvmagelo.blob.core.windows.net'
+   container_name = 'blob-container-01'
+   credential = DefaultAzureCredential(exclude_shared_token_cache_credential=True)
+   container_client = ContainerClient(account_url=storage_url, container_name=container_name, credential=credential)
+
+   try:
+      bloblist = ''
+      for blob in container_client.list_blobs():
+         bloblist += blob.name + ' '
+
+   except Exception as ex:
+      bloblist = 'error'
+
+   return render_template('list.html', list=bloblist)
 
 if __name__ == '__main__':
    app.run()
